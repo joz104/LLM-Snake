33fecd7ef244631c49aa01d180a80b5e95c1e8d4
joz104 <101255865+joz104@users.noreply.github.com>
2023-05-16 16:46:44 -0300

    ChatGPT Snake
    Snake on ChatGPT using GPT4, tested again on May 16

--- snakegpt40516.py
+++ snakegpt40516.py
@@ -1,72 +1,132 @@
+import pygame
+import time
 import random
-import curses
 
-def main(stdscr):
-    # Set up the screen
-    curses.curs_set(0)
-    stdscr.nodelay(1)
-    stdscr.timeout(100)
-    
-    sh, sw = stdscr.getmaxyx()
-    w = curses.newwin(sh, sw, 0, 0)
+# Window size
+WIDTH = 600
+HEIGHT = 400
 
-    # Set up the snake
-    snk_x = sw//4
-    snk_y = sh//2
-    snake = [
-        [snk_y, snk_x],
-        [snk_y, snk_x-1],
-        [snk_y, snk_x-2]
-    ]
+# Colors (R, G, B)
+WHITE = (255, 255, 255)
+RED = (213, 50, 80)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
 
-    # Set up the food
-    food = [sh//2, sw//2]
-    w.addch(int(food[0]), int(food[1]), curses.ACS_PI)
+# Initial snake and food position
+snake_pos = [[100, 50], [90, 50], [80, 50]]
+food_pos = [random.randrange(1, WIDTH//10)*10, random.randrange(1, HEIGHT//10)*10]
+food_spawn = True
 
-    # Initial direction is right
-    key = curses.KEY_RIGHT
+# Setting direction variables
+direction = 'RIGHT'
+change_to = direction
 
-    while True:
-        next_key = w.getch()
-        key = key if next_key == -1 else next_key
+# Initial score
+score = 0
 
-        # Check if game over
-        if snake[0][0] in [0, sh] or \
-            snake[0][1]  in [0, sw] or \
-            snake[0] in snake[1:]:
-            break
+# Pygame initialization
+pygame.init()
+pygame.display.set_caption('Snake Game')
+game_window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-        # Determine the new head based on the direction
-        new_head = [snake[0][0], snake[0][1]]
+# FPS (frames per second) controller
+fps = pygame.time.Clock()
 
-        if key == curses.KEY_DOWN:
-            new_head[0] += 1
-        if key == curses.KEY_UP:
-            new_head[0] -= 1
-        if key == curses.KEY_LEFT:
-            new_head[1] -= 1
-        if key == curses.KEY_RIGHT:
-            new_head[1] += 1
+def game_over():
+    my_font = pygame.font.SysFont('times new roman', 90)
+    game_over_surface = my_font.render('YOU DIED', True, RED)
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (WIDTH//2, HEIGHT//4)
+    game_window.fill(BLACK)
+    game_window.blit(game_over_surface, game_over_rect)
+    show_score(0, RED, 'times', 20)
+    pygame.display.flip()
+    time.sleep(3)
+    pygame.quit()
+    quit()
 
-        # Insert new head
-        snake.insert(0, new_head)
+def show_score(choice, color, font, size):
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render('Score : ' + str(score), True, color)
+    score_rect = score_surface.get_rect()
+    if choice == 1:
+        score_rect.midtop = (WIDTH//10, 15)
+    else:
+        score_rect.midtop = (WIDTH//2, HEIGHT//1.25)
+    game_window.blit(score_surface, score_rect)
 
-        # If snake has eaten the food
-        if snake[0] == food:
-            food = None
-            while food is None:
-                nf = [
-                    random.randint(1, sh-1),
-                    random.randint(1, sw-1)
-                ]
-                food = nf if nf not in snake else None
-            w.addch(food[0], food[1], curses.ACS_PI)
-        else:
-            # Remove the tail of the snake
-            tail = snake.pop()
-            w.addch(int(tail[0]), int(tail[1]), ' ')
+# Game loop
+while True:
+    # Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                change_to = 'UP'
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                change_to = 'DOWN'
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                change_to = 'LEFT'
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                change_to = 'RIGHT'
 
-        w.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
+    # Direction validation
+    if change_to == 'UP' and direction != 'DOWN':
+        direction = 'UP'
+    if change_to == 'DOWN' and direction != 'UP':
+        direction = 'DOWN'
+    if change_to == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    if change_to == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'
 
-if __name__ == "__main__":
-    curses.wrapper(main)
+    # Update snake position
+    if direction == 'UP':
+        snake_pos.insert(0, [snake_pos[0][0], snake_pos[0][1] - 10])
+    if direction == 'DOWN':
+        snake_pos.insert(0, [snake_pos[0][0], snake_pos[0][1] + 10])
+    if direction == 'LEFT':
+        snake_pos.insert(0, [snake_pos[0][0] - 10, snake_pos[0][1]])
+    if direction == 'RIGHT':
+        snake_pos.insert(0, [snake_pos[0][0] + 10, snake_pos[0][1]])
+
+    # Game over when snake is outside the screen
+    if snake_pos[0][0] >= WIDTH or snake_pos[0][0] < 0 or snake_pos[0][1] >= HEIGHT or snake_pos[0][1] < 0:
+        game_over()
+
+    # Game over when snake runs over itself
+    for pos in snake_pos[1:]:
+        if pos == snake_pos[0]:
+            game_over()
+
+    # Spawning food
+    if not food_spawn:
+        food_pos = [random.randrange(1, WIDTH//10)*10, random.randrange(1, HEIGHT//10)*10]
+    food_spawn = snake_pos[0] != food_pos
+
+    # Eating food
+    if snake_pos[0] == food_pos:
+        score += 1
+        food_spawn = False
+    else:
+        snake_pos.pop()
+
+    # Graphics
+    game_window.fill(BLACK)
+    for pos in snake_pos:
+        pygame.draw.rect(game_window, GREEN, pygame.Rect(pos[0], pos[1], 10, 10))
+    pygame.draw.rect(game_window, WHITE, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
+
+    # Game Over conditions
+    if snake_pos[0][0] >= WIDTH or snake_pos[0][0] < 0 or snake_pos[0][1] >= HEIGHT or snake_pos[0][1] < 0:
+        game_over()
+    for pos in snake_pos[1:]:
+        if pos == snake_pos[0]:
+            game_over()
+
+    show_score(1, WHITE, 'consolas', 20)
+    pygame.display.update()
+    fps.tick(24)
+
